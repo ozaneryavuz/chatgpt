49ed7160217eeed62610d4590eee1e35f4d9549e
ozaneryavuz <53664771+ozaneryavuz@users.noreply.github.com>
2026-07-28 01:02:22 +0300

    fix(alo186): Render PostgreSQL bağlantı URL'sini psycopg3 biçimine normalize et

--- alo186/sureklilik-api/entrypoint.sh
+++ alo186/sureklilik-api/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -eu
 
+case "${ALO186_DATABASE_URL:-}" in
+  postgresql://*)
+    ALO186_DATABASE_URL="postgresql+psycopg://${ALO186_DATABASE_URL#postgresql://}"
+    export ALO186_DATABASE_URL
+    ;;
+  postgres://*)
+    ALO186_DATABASE_URL="postgresql+psycopg://${ALO186_DATABASE_URL#postgres://}"
+    export ALO186_DATABASE_URL
+    ;;
+esac
+
 if [ "${ALO186_RUN_MIGRATIONS:-true}" = "true" ]; then
   alembic upgrade head
 fi
