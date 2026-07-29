f80d40e034db867cffe74f71ffc1af2c13f80a0e
ozaneryavuz <53664771+ozaneryavuz@users.noreply.github.com>
2026-07-29 19:57:32 +0300

    affiliate: add a Node-only catalog shim for build-time graph generation

---
+++ alo186/urun-bilgi-grafigi/catalog.js
@@ -0,0 +1,5 @@
+(function(root){
+  'use strict';
+  if(typeof module==='object'&&module.exports){module.exports=require('../akilli-urun-secimi/catalog.js');return;}
+  if(!root.Alo186ProductCatalog)throw new Error('ALO186 ürün kataloğu önce yüklenmelidir.');
+})(typeof globalThis!=='undefined'?globalThis:this);
