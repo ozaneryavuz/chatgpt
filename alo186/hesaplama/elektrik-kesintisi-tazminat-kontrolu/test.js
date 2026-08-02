06250bb2d8076ad18aafe3d8ec0c4ce8ec88a16a
ozaneryavuz <53664771+ozaneryavuz@users.noreply.github.com>
2026-08-02 21:31:35 +0300

    ALO186: tazminat aracını 30 gün mevzuatına kilitle

--- alo186/hesaplama/elektrik-kesintisi-tazminat-kontrolu/test.js
+++ alo186/hesaplama/elektrik-kesintisi-tazminat-kontrolu/test.js
@@ -1,1 +1,29 @@
-const fs=require('fs');const path=require('path');const html=fs.readFileSync(path.join(__dirname,'index.html'),'utf8');for(const token of ['id="checker"','id="result"','12 saati','on iş günü','FAQPage','WebApplication','/edas-bul']){if(!html.includes(token))throw new Error(`Eksik sözleşme: ${token}`);}if(!/addEventListener\('submit'/.test(html))throw new Error('Karar motoru submit olayı eksik');console.log('outage compensation checker: PASS');
+const fs = require('fs');
+const path = require('path');
+
+const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+
+for (const token of [
+  'id="checker"',
+  'id="result"',
+  '12 saati',
+  '30 gün',
+  'FAQPage',
+  'WebApplication',
+  '/edas-bul',
+  'data-evidence-download',
+  'outage_compensation_result'
+]) {
+  if (!html.includes(token)) throw new Error(`Eksik sözleşme: ${token}`);
+}
+
+if (/on iş günü|10 iş günü|Ten Business Days/i.test(html)) {
+  throw new Error('Cihaz hasarı başvurusunda eski 10 iş günü ifadesi bulundu');
+}
+if (/"@type"\s*:\s*"Offer"/.test(html)) {
+  throw new Error('Araç sayfasında Offer şeması kullanılmamalı');
+}
+if (!/addEventListener\('submit'/.test(html)) throw new Error('Karar motoru submit olayı eksik');
+if (!/Blob\(/.test(html)) throw new Error('Kanıt dosyası indirme işlevi eksik');
+
+console.log('outage compensation checker: PASS');
